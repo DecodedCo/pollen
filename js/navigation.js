cc338ec1449182ea1b170ea31d71009a3b451317
Jonas Haefele <jonas@decoded.co>
2014-02-18 12:43:55 +0000

    add global nav collapse script

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-    $(".globalnav a").click(function(event) {
-    	var $toggle = $(this).parents('.global-nav').find('navbar-toggle');
+  function updateGlobalNavCollapse(){
+    $(".global-nav li a").click(function(event) {
+    	var $toggle = $(this).parents('.global-nav').find('.navbar-toggle');
     	var $target = $($toggle.data('target')); 
         // check if window is small enough so dropdown is created
         $toggle.is(":visible")
-            $target.toggle();
+            $target.removeClass('in');
+            $target.addClass('collapse');
     });
+  }
+  updateGlobalNavCollapse();
 });
