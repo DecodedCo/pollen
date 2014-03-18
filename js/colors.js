fe7ac5a9a16c762c4c29a4c31f92589b738f4031
Jonas Haefele <jonas@decoded.co>
2014-03-18 13:27:23 +0000

    add colors.js Closes #8

---
+++ js/colors.js
@@ -0,0 +1,33 @@
+var colorDecodedYellow = 'rgb(254,248,0)';
+
+var secondaryColorsNamed = {
+  'green': 'rgb(46,204,133)',
+  'blue':  'rgb(52,141,204)',
+  'red':   'rgb(231,76,60)',
+  'orange':'rgb(243,146,15)',
+  'pink':  'rgb(249,58,142)',
+  'purple':'rgb(140,86,182)'};
+
+var secondaryColors = [
+  'rgb(46,204,133)',
+  'rgb(52,141,204)',
+  'rgb(231,76,60)',
+  'rgb(243,146,15)',
+  'rgb(249,58,142)',
+  'rgb(140,86,182)'];
+
+var secondaryColorsLightNamed = {
+  'green-light':  'rgb(192,227,139)',
+  'blue-light':   'rgb(127,195,225)',
+  'red-light':    'rgb(253,158,127)',
+  'orange-light': 'rgb(255,202,128)',
+  'pink-light':   'rgb(252,131,179)',
+  'purple-light': 'rgb(171,127,193)'};
+
+var secondaryColorsLight = [
+  'rgb(192,227,139)',
+  'rgb(127,195,225)',
+  'rgb(253,158,127)',
+  'rgb(255,202,128)',
+  'rgb(252,131,179)',
+  'rgb(171,127,193)'];
