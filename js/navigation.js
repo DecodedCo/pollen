2adfd086fd02132a2e1bfed5d30baca3d9bee214
Jonas Haefele <jonas@decoded.co>
2014-03-19 17:03:09 +0000

    add dropdown styles for navbar. Closes #15

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
   function updateGlobalNavCollapse(){
     $(".global-nav li a").click(function(event) {
-    	var $toggle = $(this).parents('.global-nav').find('.navbar-toggle');
-    	var $target = $($toggle.data('target')); 
-        // check if window is small enough so dropdown is created
-        $toggle.is(":visible")
-            $target.removeClass('in');
-            $target.addClass('collapse');
+      if($(this).parent().hasClass('dropdown')){
+        console.log('dont collapse, its a dropdown')
+      }else{
+        var $toggle = $(this).parents('.global-nav').find('.navbar-toggle');
+        var $target = $($toggle.data('target'));
+          // check if window is small enough so dropdown is created
+          $toggle.is(":visible")
+              $target.removeClass('in');
+              $target.addClass('collapse');
+      }
     });
   }
   updateGlobalNavCollapse();
