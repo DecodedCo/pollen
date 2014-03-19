b57282e8495f13d10fca0587e1d31c446bc732fc
Jonas Haefele <jonas@decoded.co>
2014-03-19 17:07:25 +0000

    remove console.log

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   function updateGlobalNavCollapse(){
     $(".global-nav li a").click(function(event) {
-      if($(this).parent().hasClass('dropdown')){
-        console.log('dont collapse, its a dropdown')
-      }else{
+      // only collapse if it's not a dropdown
+      if(!$(this).parent().hasClass('dropdown')){
         var $toggle = $(this).parents('.global-nav').find('.navbar-toggle');
         var $target = $($toggle.data('target'));
           // check if window is small enough so dropdown is created
